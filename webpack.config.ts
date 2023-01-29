5670731b0d5c0f87064d14a9def3114521151d2e
Misha <wrongsky1@yandex.ru>
2023-01-29 17:03:15 +0300

    step 2 buildWebpackConfig decompose

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,33 +1,23 @@
+
 import path from "path";
-import HTMLWebpackPlugin from "html-webpack-plugin";
 import webpack from "webpack";
 
-const config: webpack.Configuration = {
-    mode: "development",
+import { buildWebpackConfig } from "./config/build/buildWebpackConfig";
+import { BuildPaths } from "./config/build/types/config";
+
+const paths: BuildPaths = {
     entry: path.resolve(__dirname, "src", "index.ts"),
-    output: {
-        filename: "[name].[contenthash].js",
-        path: path.resolve(__dirname, "build"),
-        clean: true,
-    },
-    plugins: [
-        new webpack.ProgressPlugin(),
-        new HTMLWebpackPlugin({
-            template: path.resolve(__dirname, "public", "index.html"),
-        }),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: "ts-loader",
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: [".tsx", ".ts", ".js"],
-    },
-};
+    build: path.resolve(__dirname, "build"),
+    html: path.resolve(__dirname, "public", "index.html"),
+}
+
+const mode = 'development';
+const isDev = mode === 'development';
+
+const config: webpack.Configuration = buildWebpackConfig({
+    mode,
+    paths,
+    isDev,
+});
 
 export default config;
