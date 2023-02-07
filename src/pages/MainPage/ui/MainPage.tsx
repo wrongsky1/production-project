80a43814f5cdfb0208c400c06550f46f27f6bda2
Misha <wrongsky1@yandex.ru>
2023-02-07 11:47:29 +0300

    st15 add i18n for translate site content

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 
 const MainPage = () => {
+  const {t} = useTranslation()
+
   return (
-    <div>MainPage</div>
+    <div>{t('Главная страница')}</div>
   )
 }
 
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { classNames } from 'shared/lib/classNames'
+import { LangSwitcher } from 'widgets/LangSwitcher'
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher'
 import cls from './Sidebar.module.scss'
  
@@ -21,6 +22,7 @@ const Sidebar = ({className}: PropsSidebar) => {
       <button onClick={onToggle}>toggle</button>
       <div className={cls.switchers}>
         <ThemeSwitcher/>
+        <LangSwitcher className={cls.lang}/>
       </div>
     </div>
   )
