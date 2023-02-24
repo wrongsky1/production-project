8fa60260259c1843f8e7f4ffe4dcf55812a0693c
Misha <wrongsky1@yandex.ru>
2023-02-24 23:34:28 +0300

    st34 add auth: reducers, slices, async thunk; custom component text; refact loginForm

--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,5 +1,5 @@
-import { classNames } from 'shared/lib/classNames/classNames';
 import { Modal } from 'shared/ui/Modal/Modal';
+import { classNames } from 'shared/lib/classNames/classNames';
 import { LoginForm } from '../LoginForm/LoginForm';
 
 interface LoginModalProps {
