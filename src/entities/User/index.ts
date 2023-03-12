7bd7eaeadaa16fde83b478c1c9dcfa5a7316950e
Misha <wrongsky1@yandex.ru>
2023-03-12 16:28:38 +0300

    st47 add private protected routes

--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -1,7 +1,9 @@
 export {
     getUserAuthData,
 } from './model/selectors/getUserAuthData/getUserAuthData';
-
+export {
+    getUserInited,
+} from './model/selectors/getUserInited/getUserInited';
 export {
     userReducer,
     userActions,
