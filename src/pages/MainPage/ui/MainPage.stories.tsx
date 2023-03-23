38d3e9ad04982e286f7e3ea34d0759ddc28cfd84
Misha <wrongsky1@yandex.ru>
2023-03-23 11:54:33 +0300

    st56 add pagination; observer API and hook infiniteScroll

--- src/pages/MainPage/ui/MainPage.stories.tsx
+++ src/pages/MainPage/ui/MainPage.stories.tsx
@@ -12,7 +12,6 @@ export default {
     },
 } as ComponentMeta<typeof MainPage>;
 
-// eslint-disable-next-line react/jsx-props-no-spreading
 const Template: ComponentStory<typeof MainPage> = () => <MainPage />;
 
 export const Normal = Template.bind({});
