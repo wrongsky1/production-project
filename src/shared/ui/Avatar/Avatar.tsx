1673cce90e1f56a319acf8cf23733ab3b88ecfc2
Misha <wrongsky1@yandex.ru>
2023-05-23 10:59:33 +0300

    st106 add appImage component, fallback and errorFallback

--- src/shared/ui/Avatar/Avatar.tsx
+++ src/shared/ui/Avatar/Avatar.tsx
@@ -1,26 +1,36 @@
 import { CSSProperties, useMemo } from 'react';
 import { classNames, Mods } from '@/shared/lib/classNames/classNames';
 import cls from './Avatar.module.scss';
+import UserIcon from '../../assets/icons/user-filled.svg';
+import { AppImage } from '../AppImage';
+import { Skeleton } from '../Skeleton';
+import { Icon } from '../Icon';
 
 interface AvatarProps {
     className?: string;
     src?: string;
     size?: number;
     alt?: string;
+    fallbackInverted?: boolean;
 }
 
 export const Avatar = ({
-    className, src, size, alt,
+    className, src, size, alt, fallbackInverted,
 }: AvatarProps) => {
     const mods: Mods = {};
 
     const styles = useMemo<CSSProperties>(() => ({
-        width: size || 100,
-        height: size || 100,
+        width: size,
+        height: size,
     }), [size]);
 
+    const fallback = <Skeleton width={size} height={size} border="50%" />;
+    const errorFallback = <Icon inverted={fallbackInverted} width={size} height={size} Svg={UserIcon} />;
+
     return (
-        <img
+        <AppImage
+            fallback={fallback}
+            errorFallback={errorFallback}
             src={src}
             alt={alt}
             style={styles}
