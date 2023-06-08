6dad068e1f7bd19455cb97e0f14717887534b555
Misha <wrongsky1@yandex.ru>
2023-06-08 19:13:29 +0300

    st128 init auth data from server

--- src/entities/User/model/services/saveJsonSettings.ts
+++ src/entities/User/model/services/saveJsonSettings.ts
@@ -3,7 +3,7 @@ import { ThunkConfig } from '@/app/providers/StoreProvider';
 import { JsonSettings } from '../types/jsonSettings';
 import { getUserAuthData } from '../selectors/getUserAuthData/getUserAuthData';
 import { getJsonSettings } from '../selectors/jsonSettings';
-import { setJsonSettingsMutation } from '../../apiUser/userApi';
+import { setJsonSettingsMutation } from '../../api/userApi';
 
 export const saveJsonSettings = createAsyncThunk<
     JsonSettings,
