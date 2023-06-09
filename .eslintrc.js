8d31638b7eb9e350ce4729c8f3178ede5e803499
Misha <wrongsky1@yandex.ru>
2023-06-09 15:21:24 +0300

    st130 add toggleFeature for switching components

--- .eslintrc.js
+++ .eslintrc.js
@@ -70,6 +70,7 @@ module.exports = {
                     'direction',
                     'gap',
                     'max',
+                    'feature',
                 ],
             },
         ],
