0ae2f8dee5ea97b9ae5b9b494f2dea162875e6ba
Misha <wrongsky1@yandex.ru>
2023-06-15 17:26:38 +0300

    st142 add ui design switcher, mutation for change feature flags

--- src/features/avatarDropdown/ui/AvatarDropdown/AvatarDropdown.tsx
+++ src/features/avatarDropdown/ui/AvatarDropdown/AvatarDropdown.tsx
@@ -10,7 +10,7 @@ import {
     isUserManager,
     userActions,
 } from '@/entities/User';
-import { getRouteAdmin, getRouteProfile } from '@/shared/const/router';
+import { getRouteAdmin, getRouteProfile, getRouteSettings } from '@/shared/const/router';
 import { ToggleFeatures } from '@/shared/lib/features';
 import { Dropdown } from '@/shared/ui/redesigned/Popups';
 import { Avatar } from '@/shared/ui/redesigned/Avatar';
@@ -40,12 +40,16 @@ export const AvatarDropdown = memo((props: AvatarDropdownProps) => {
     const items = [
         ...(isAdminPanelAvailable
             ? [
-                  {
-                      content: t('Админка'),
-                      href: getRouteAdmin(),
-                  },
-              ]
+                {
+                    content: t('Админка'),
+                    href: getRouteAdmin(),
+                },
+            ]
             : []),
+        {
+            content: t('Настройки'),
+            href: getRouteSettings(),
+        },
         {
             content: t('Профиль'),
             href: getRouteProfile(authData.id),
