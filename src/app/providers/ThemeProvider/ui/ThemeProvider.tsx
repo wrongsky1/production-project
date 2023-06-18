c5f2e7be33257f7a967f1b04bedcafa7eb36bf1a
Misha <wrongsky1@yandex.ru>
2023-06-18 17:01:47 +0300

    st150 add loading layout, fallback theme, default features

--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -2,11 +2,14 @@ import React, { ReactNode, useEffect, useMemo, useState } from 'react';
 import { ThemeContext } from '../../../../shared/lib/context/ThemeContext';
 import { Theme } from '@/shared/const/theme';
 import { useJsonSettings } from '@/entities/User';
+import { LOCAL_STORAGE_THEME_KEY } from '@/shared/const/localstorage';
 
 interface ThemeProviderProps {
     initialTheme?: Theme;
     children: ReactNode;
 }
+
+const fallbackTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme;
 
 const ThemeProvider = (props: ThemeProviderProps) => {
     const { initialTheme, children } = props;
@@ -14,7 +17,7 @@ const ThemeProvider = (props: ThemeProviderProps) => {
     const [isThemeInited, setThemeInited] = useState(false);
 
     const [theme, setTheme] = useState<Theme>(
-        initialTheme || defaultTheme || Theme.LIGHT,
+        initialTheme || fallbackTheme || Theme.LIGHT,
     );
 
     useEffect(() => {
@@ -25,8 +28,9 @@ const ThemeProvider = (props: ThemeProviderProps) => {
     }, [defaultTheme, isThemeInited]);
 
     useEffect(() => {
-        document.body.className = theme
-    }, [theme])
+        document.body.className = theme;
+        localStorage.setItem(LOCAL_STORAGE_THEME_KEY, theme);
+    }, [theme]);
 
     const defaultProps = useMemo(
         () => ({
--- src/shared/lib/features/services/updateFeatureFlags.ts
+++ src/shared/lib/features/services/updateFeatureFlags.ts
@@ -30,7 +30,7 @@ export const updateFeatureFlag = createAsyncThunk<
         );
 
         setFeatureFlags(allFeatures);
-        window.location.reload()
+        window.location.reload();
         return undefined;
     } catch (e) {
         console.log(e);
